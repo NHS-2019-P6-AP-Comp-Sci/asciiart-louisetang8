cba254db17b863186404f54c774e17ef38b70eb1
s-tanglo <s-tanglo@53S50029447.it.bsd405.org>
2019-09-20 13:35:36 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,27 +1,61 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
-
-// YOU CODE HERE :) 
-
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+
+	public static void main(String[] args) {
+		star();
+		smallStar();
+		System.out.print("*");
+		smallStar();
+		horizontalEars();
+		verticalEars();
+		verticalEars();
+		face();
+		body();
+		legs();
+		legs();
+		feet();
+	}
+
+	public static void star() {
+		System.out.println(" *   *    *   /\\   *   *");
+		System.out.println("   *    *   <    >   *");
+		System.out.println(" *   *   *   \\/\\/  *    *");
+	}
+
+	public static void smallStar() {
+		System.out.println("  *   *    *    *    *  ");
+	}
+
+	public static void horizontalEars() {
+		System.out.print("  +--+");
+		System.out.print(" ");
+		System.out.print("+--+\n");
+	}
+
+	public static void verticalEars() {
+		System.out.print("  |  |");
+		System.out.print(" ");
+		System.out.print("|  |\n");
+	}
+
+	public static void face() {
+		System.out.println(" _|  |_|  |_       ________________________");
+		System.out.println("|           |     <\"wow look at the stars!\"> ");
+		System.out.println("|   > - <   |    / ------------------------");
+		System.out.println("| \"   ~   \" |   /");
+		System.out.println("|  _     _  |");
+		System.out.println("|_| |_ _| |_|");
+	}
+
+	public static void body() {
+		System.out.println("  |_|   |_| ");
+		System.out.println("    |_____|O");
+	}
+
+	public static void legs() {
+		System.out.println("      |  |");
+	}
+
+	public static void feet() {
+		System.out.print("     _| _|");
 	}
 }
